7a2f11dd35499ce028664bcad1e8648122024367
Pantelis <kouridakispantelis@gmail.com>
2025-09-14 11:34:32 +0300

    Update UI

--- app/about/experience-data.tsx
+++ app/about/experience-data.tsx
@@ -13,31 +13,31 @@ export const experienceData: {
   experience: [
     {
       title: "AI Developer",
-      period: "Feb 2025 - present",
+      period: "Feb 2025 - Present",
       description: "Prometheus Space Technologies",
       location: "London, United Kingdom",
-      techStack: ["Python", "Scikit-learn", "PostgreSQL", "Rest APIs"],
+      techStack: ["Python", "Scikit-learn", "React.js", "PostgreSQL", "Rest APIs"],
     },
     {
       title: "RPA Engineer",
       period: "May 2024 - Present",
       description: "Generali Hellas",
       location: "Athens, Greece",
-      techStack: ["Power Platform", "Python", "Automation"],
+      techStack: ["Power Platform", "Python", "OpenCV"],
     },
     {
       title: "RPA Developer",
       period: "Dec 2022 - May 2024",
       description: "Aambience Services",
       location: "Athens, Greece",
-      techStack: ["Power Platform", "Python", "Automation"],
+      techStack: ["Power Platform", "Python", "SQL", "Power BI"],
     },
     {
       title: "SAP Consultant",
       period: "Oct 2021 - Dec 2022",
       description: "GVision",
       location: "Athens, Greece",
-      techStack: ["SAP B1", "C#", "BI"],
+      techStack: ["SAP B1", "SQL"],
     },
   ],
   education: [
